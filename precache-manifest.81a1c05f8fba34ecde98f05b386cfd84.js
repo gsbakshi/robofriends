93c0d0f9a8417032b88d6bbe64bebaa7e72bc44d
guribakshi <gurubakshi486@gmail.com>
2020-05-27 10:31:13 +0530

    Updates

---
+++ precache-manifest.81a1c05f8fba34ecde98f05b386cfd84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8111912a555240d74d56a13cdcd295",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6a5e9bd9ebd9724843bc",
+    "url": "/robofriends/static/css/main.0f944487.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5e9bd9ebd9724843bc",
+    "url": "/robofriends/static/js/main.f46423e6.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
